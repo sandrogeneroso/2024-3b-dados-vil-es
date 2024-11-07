07c3c047480fd54b23aacc693276910e9804aea3
Sandro Generoso <106679349+sandrogeneroso@users.noreply.github.com>
2024-11-07 10:25:23 -0300

    Add files via upload

--- graficos/dadosGerais.js
+++ graficos/dadosGerais.js
@@ -1,16 +1,23 @@
 const url="https://raw.githubusercontent.com/sandrogeneroso/2024-API-VIL-ES/refs/heads/main/api-VIL%C3%95ES-.JSON"
 
 async function vizualizainfo(){
-    const res=await fetch(url)
-    const dados= await res.json()
+    const res = await fetch(url)
+    const dados = await res.json()
 
-    console.log(dados)
+
     const viloes=Object.keys(dados)
     const viloesMaisVotados=viloes[0]
     const quantidadedeVotos=Object.values(dados)[0]
 
-    console.log(viloesMaisVotados)
-    console.log(quantidadedeVotos)
+    let paragrafo = document.createElement('p')
+
+    paragrafo.classList.add('caixa-grafico__texto')
+    
+    paragrafo.innerHTML = ` Nessa pesquisa, buscou-se compreender qual o vilão mais votados pelos jovens. O ${viloesMaisVotados} foi o vilão mais votado entre todos, com um total de ${quantidadedeVotos} votos, em uma pesquisa que teve um total de um milhão de participantes.`
+
+    let caixa = document.getElementById('caixa-grafico')
+    caixa.appendChild(paragrafo)
+
 }
 
-vizualizainfo()
+vizualizaInfo()
